0726b80a86fd7c364da0f00b740b97527508e155
imad jeghalef <imadjeghalef@datepalm.media>
2023-09-12 11:07:13 -0400

    combine the fronend and backend

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const port = 3000;
 const routes = require("./routes/index.js");
 var cors = require("cors");
+const path = __dirname + '/views/';
+app.use(express.static(path));
 app.use(cors({
   origin: 'http://localhost:8080',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
 }));
+app.get('/', function (req,res) {
+  res.sendFile(path + "index.html");
+});
 app.use(express.json({limit: '50mb'}));
 const bodyParser = require("body-parser");
 // parse application/json
