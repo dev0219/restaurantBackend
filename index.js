4d926eade1a31479bb0b26de6639f131342ffa43
imad jeghalef <imadjeghalef@datepalm.media>
2023-09-28 03:39:01 -0400

    update the cors

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const app = express();
 const port = 3000;
 const routes = require("./routes/index.js");
 var cors = require("cors");
-const path = __dirname + '/views/';
-app.use(express.static(path));
+// const path = __dirname + '/views/';
+// app.use(express.static(path));
 app.use(cors({
-  origin: '/',
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
 }));
 
-app.get('*', function (req,res) {
-  res.sendFile(path + "index.html");
-});
+// app.get('*', function (req,res) {
+//   res.sendFile(path + "index.html");
+// });
 app.use(express.json({limit: '50mb'}));
 const bodyParser = require("body-parser");
 // parse application/json
